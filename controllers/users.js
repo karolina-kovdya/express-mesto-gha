fe33305638b4511ed43d9cfcc8cbae7952b95448
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2023-03-06 09:25:19 +0300

    add errorStatus.js && refactoring err

--- controllers/users.js
+++ controllers/users.js
@@ -1,62 +1,61 @@
 const User = require('../models/users');
+const { statusError, statusSucces } = require('../errorStatus');
 
-const createUser = async (req, res) => {
+const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   return User.create({ name, about, avatar })
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(statusSucces.CREATED).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
+        res.status(statusError.BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
 
         return;
       }
-      res.status(500).send({ message: err.message });
+      res.status(statusError.SERVER_ERROR).send({ message: 'Произошла ошибка' });
     });
 };
 
 const getUser = (req, res) => {
-  User.findById(req.params.id)
+  const { userID } = req.params.id;
+
+  User.findById(userID)
     .then((user) => {
       if (!user) {
-        res.status(400).send({ message: 'Переданы некорректные данные, пользователь не найден' });
+        res.status(statusError.NOT_FOUND).send({ message: 'Пользователь по указанному id не найден' });
 
         return;
       }
-      res.status(200).send(user);
+      res.status(statusSucces.OK).send(user);
     })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(statusError.BAD_REQUEST).send({ message: 'Передан некорректный id' });
+
+        return;
+      }
+      res.status(statusError.SERVER_ERROR).send({ message: 'Произошла ошибка' });
+    });
 };
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => {
-      if (!users) {
-        res.status(404).send({ message: 'Пользователи не найдены' });
-
-        return;
-      }
-      res.status(200).send(users);
-    })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(statusSucces.OK).send(users))
+    .catch((err) => res.status(statusError.SERVER_ERROR).send({ message: `Произошла ошибка ${err.status}` }));
 };
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
 
   User.findByIdAndUpdate(req.user_id, { name, about }, { new: true })
-    .then((user) => res.send(user))
+    .then((user) => res.status(statusSucces.OK).send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
-
-        return;
-      } if (err.name === 'Not Found') {
-        res.status(400).send({ message: 'Пользователь не найден' });
+        res.status(statusError.BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
 
         return;
       }
-      res.status(500).send({ message: err.message });
+      res.status(statusError.SERVER_ERROR).send({ message: 'Произошла ошибка' });
     });
 };
 
@@ -64,18 +63,14 @@ const updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(req.params.id, { avatar }, { new: true })
-    .then((user) => res.send(user))
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
-
-        return;
-      } if (err.name === 'Not Found') {
-        res.status(404).send({ message: 'Пользователь не найден' });
+        res.status(statusError.BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
 
         return;
       }
-      res.status(500).send({ message: err.message });
+      res.status(statusError.SERVER_ERROR).send({ message: 'Произошла ошибка' });
     });
 };
 
---
+++ errorStatus.js
@@ -0,0 +1,14 @@
+const statusError = {
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500,
+};
+
+const statusSucces = {
+  OK: 200,
+  CREATED: 201,
+};
+
+module.exports = {
+  statusError, statusSucces,
+};
