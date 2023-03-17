411d954ce73c6b5f74dbdeea2c5ca898e1c6067d
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2023-03-17 18:32:28 +0300

    add validation with joi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { createUser, loginUser } = require('./controllers/users');
@@ -12,8 +13,21 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
-app.post('/signin', loginUser);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), loginUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 app.use(auth);
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
